4b28ad8f275eac47c104c5b61d96f0ecac871224
Jordi <jordimimbrera@gmail.com>
2018-03-21 22:59:27 +0100

    Hanoi first solution recursive

--- Models/Entities/Hanoi.cs
+++ Models/Entities/Hanoi.cs
@@ -14,7 +14,7 @@ namespace HanoiGame.Models.Entities
 
         public int total = 0;
 
-        public Hanoi() {  _1.Push(4); _1.Push(3); _1.Push(2); _1.Push(1); }
+        public Hanoi() { _1.Push(6); _1.Push(5); _1.Push(4); _1.Push(3); _1.Push(2); _1.Push(1); }
 
         public void start() { take(1); }
 
@@ -45,7 +45,7 @@ namespace HanoiGame.Models.Entities
 
         }
 
-        public void take(int movimiento, int divisor = 1, int valor = 1)
+        public void take(int movimiento)
         {
             if (_1.Count == 0 && (_2.Count == 0 || _3.Count == 0))
             {
@@ -53,15 +53,34 @@ namespace HanoiGame.Models.Entities
             }
             else
             {
-                if (odd(movimiento / divisor)) {
-                    put(valor);
-                    take(movimiento + 1);
+                if (odd(movimiento)) put(1); 
+                else
+                {
+                    if (odd(movimiento / 2))
+                    {
+                        put(2);
+                    }
+                    else
+                    {
+                        put(highest());
+                    }
                 }
-                else take(movimiento, 2 * valor, valor + 1);
+
+                take(movimiento + 1);
             }
 
         }
 
+
+        public int highest()
+        {
+            List<int> array = new List<int>();
+            array.Add( _1.Count == 0 ? 0 : (int)_1.Peek() );
+            array.Add( _2.Count == 0 ? 0 : (int)_2.Peek() );
+            array.Add( _3.Count == 0 ? 0 : (int)_3.Peek() );
+
+            return array.Where(x => x > 2).Min();
+        }
         public int findPalo(int number) { return _1.Contains(number) ? 1 : (_2.Contains(number) ? 2 : 3); }
         public Stack getPalo(int index) { return index == 1 ? _1 : (index == 2 ? _2 : _3); }
         public bool even(int number) { return (number % 2 == 0); }
